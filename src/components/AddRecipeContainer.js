15cd36a1e41b1c88527e1d71d6547ef329616d96
Joe Dodd <joehdodd@gmail.com>
2020-01-01 17:57:12 -0500

    WIP begins adding logic to add or remove ingredients and instructions

--- src/components/AddRecipeContainer.js
+++ src/components/AddRecipeContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { APIContext } from "./APIContext";
-import { withRouter } from "react-router-dom"
+import RecipeForm from "./RecipeForm";
+import { withRouter } from "react-router-dom";
 
 export default withRouter(({ location, history }) => {
   const api = React.useContext(APIContext);
@@ -28,87 +29,22 @@ export default withRouter(({ location, history }) => {
       }
     }
   );
+  const handleSubmit = e => {
+    e.preventDefault();
+    return api
+      .fetch("/recipes", {
+        method: "POST",
+        data: {
+          ...state.inputValues
+        }
+      })
+      .then(res => {
+        history.push("/");
+      });
+  };
   return (
     <div className="content-section">
-      <form
-        className="grid-form-row"
-        onSubmit={e => {
-          e.preventDefault();
-          return api
-            .fetch("/recipes", {
-              method: "POST",
-              data: {
-                ...state.inputValues
-              }
-            })
-            .then(res => {
-              history.push("/");
-            });
-        }}
-      >
-        <label htmlFor="title">Title</label>
-        <input
-          required
-          type="text"
-          name="title"
-          value={state.inputValues.title}
-          onChange={e => {
-            console.log("input", e.target.name, e.target.value);
-            dispatch({
-              type: "on-change",
-              name: e.target.name,
-              value: e.target.value
-            });
-          }}
-        />
-        <label htmlFor="description">Description</label>
-        <textarea
-          required
-          name="description"
-          value={state.inputValues.description}
-          onChange={e => {
-            console.log("input", e.target.name, e.target.value);
-            dispatch({
-              type: "on-change",
-              name: e.target.name,
-              value: e.target.value
-            });
-          }}
-        />
-        <label htmlFor="ingredients">Ingredients</label>
-        <textarea
-          required
-          placeholder="Please list each ingredient separated by a comma"
-          name="ingredients"
-          value={state.inputValues.ingredients}
-          onChange={e => {
-            console.log("input", e.target.name, e.target.value);
-            dispatch({
-              type: "on-change",
-              name: e.target.name,
-              value: e.target.value
-            });
-          }}
-        />
-        <label htmlFor="instructions">Instructions</label>
-        <textarea
-          required
-          placeholder="Please list each step separated by a comma"
-          name="instructions"
-          value={state.inputValues.instructions}
-          onChange={e => {
-            console.log("input", e.target.name, e.target.value);
-            dispatch({
-              type: "on-change",
-              name: e.target.name,
-              value: e.target.value
-            });
-          }}
-        />
-        <button className="button-quarter" type="submit">
-          Submit
-        </button>
-      </form>
+      <RecipeForm handleSubmit={handleSubmit} />
     </div>
   );
 });
--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -1,114 +1,158 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 
-export default withRouter(
-  ({ handleSubmit, inputValues }) => {
-    React.useEffect(() => {
-      Object.entries(inputValues).forEach(entry => {
-        return dispatch({
-          type: "on-change",
-          name: entry[0],
-          value: entry[1]
-        });
+export default withRouter(({ handleSubmit, inputValues = {} }) => {
+  React.useEffect(() => {
+    Object.entries(inputValues).forEach(entry => {
+      return dispatch({
+        type: "on-change",
+        name: entry[0],
+        value: entry[1]
       });
-    }, [inputValues]);
-    const [state, dispatch] = React.useReducer(
-      (state, action) => {
-        switch (action.type) {
-          case "on-change":
-            return {
-              ...state,
-              inputValues: {
-                ...state.inputValues,
-                [action.name]: action.value
-              }
-            };
-          default:
-            return state;
-        }
-      },
-      {
-        inputValues: {
-          title: "",
-          description: "",
-          ingredients: "",
-          instructions: ""
-        }
+    });
+  }, [inputValues]);
+  const [state, dispatch] = React.useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case "on-change":
+          return {
+            ...state,
+            inputValues: {
+              ...state.inputValues,
+              [action.name]: action.value
+            }
+          };
+        case "increment-ing-count":
+          return {
+            ...state,
+            ingredientCount: state.ingredientCount + 1
+          };
+        case "decrement-ing-count":
+          return {
+            ...state,
+            ingredientCount:
+              state.ingredientCount > 1
+                ? state.ingredientCount - 1
+                : state.ingredientCount
+          };
+        default:
+          return state;
       }
-    );
-    return (
-      <div className="content-section">
-        <form
-          className="grid-form-row"
-          onSubmit={e => {
+    },
+    {
+      ingredientCount: 1,
+      instructionCount: 1,
+      inputValues: {
+        title: "",
+        description: "",
+        ingredients: "",
+        instructions: ""
+      }
+    }
+  );
+  return (
+    <div className="content-section">
+      <form className="grid-form-row">
+        <label htmlFor="title">Title</label>
+        <input
+          required
+          type="text"
+          name="title"
+          value={state.inputValues.title}
+          onChange={e => {
+            console.log("input", e.target.name, e.target.value);
+            dispatch({
+              type: "on-change",
+              name: e.target.name,
+              value: e.target.value
+            });
+          }}
+        />
+        <label htmlFor="description">Description</label>
+        <textarea
+          required
+          name="description"
+          value={state.inputValues.description}
+          onChange={e => {
+            console.log("input", e.target.name, e.target.value);
+            dispatch({
+              type: "on-change",
+              name: e.target.name,
+              value: e.target.value
+            });
+          }}
+        />
+        <label htmlFor="ingredients">Ingredients</label>
+        {Array(state.ingredientCount)
+          .fill(null)
+          .map((_, i) => (
+            <div className="form-ing-inst-grid">
+              <span>{i + 1}.</span>
+              <textarea
+                required
+                placeholder="Please list one ingredient per field"
+                name="ingredients"
+                value={state.inputValues.ingredients}
+                onChange={e => {
+                  console.log("input", e.target.name, e.target.value);
+                  dispatch({
+                    type: "on-change",
+                    name: e.target.name,
+                    value: e.target.value
+                  });
+                }}
+              />
+            </div>
+          ))}
+        <div>
+          <button
+            className="form-minus-button"
+            onClick={e => {
+              e.stopPropagation();
+              e.preventDefault();
+              dispatch({ type: "decrement-ing-count" });
+            }}
+          >
+            -
+          </button>
+          <button
+            className="form-plus-button"
+            onClick={e => {
+              e.stopPropagation();
+              e.preventDefault();
+              dispatch({ type: "increment-ing-count" });
+            }}
+          >
+            +
+          </button>
+        </div>
+        <label htmlFor="instructions">Instructions</label>
+        <textarea
+          required
+          placeholder="Please list each step separated by a comma"
+          name="instructions"
+          value={state.inputValues.instructions}
+          onChange={e => {
+            console.log("input", e.target.name, e.target.value);
+            dispatch({
+              type: "on-change",
+              name: e.target.name,
+              value: e.target.value
+            });
+          }}
+        />
+        <button
+          onClick={e => {
+            e.stopPropagation();
             e.preventDefault();
             return handleSubmit(state.inputValues);
           }}
+          className="button-quarter"
+          type="submit"
         >
-          <label htmlFor="title">Title</label>
-          <input
-            required
-            type="text"
-            name="title"
-            value={state.inputValues.title}
-            onChange={e => {
-              console.log("input", e.target.name, e.target.value);
-              dispatch({
-                type: "on-change",
-                name: e.target.name,
-                value: e.target.value
-              });
-            }}
-          />
-          <label htmlFor="description">Description</label>
-          <textarea
-            required
-            name="description"
-            value={state.inputValues.description}
-            onChange={e => {
-              console.log("input", e.target.name, e.target.value);
-              dispatch({
-                type: "on-change",
-                name: e.target.name,
-                value: e.target.value
-              });
-            }}
-          />
-          <label htmlFor="ingredients">Ingredients</label>
-          <textarea
-            required
-            placeholder="Please list each ingredient separated by a comma"
-            name="ingredients"
-            value={state.inputValues.ingredients}
-            onChange={e => {
-              console.log("input", e.target.name, e.target.value);
-              dispatch({
-                type: "on-change",
-                name: e.target.name,
-                value: e.target.value
-              });
-            }}
-          />
-          <label htmlFor="instructions">Instructions</label>
-          <textarea
-            required
-            placeholder="Please list each step separated by a comma"
-            name="instructions"
-            value={state.inputValues.instructions}
-            onChange={e => {
-              console.log("input", e.target.name, e.target.value);
-              dispatch({
-                type: "on-change",
-                name: e.target.name,
-                value: e.target.value
-              });
-            }}
-          />
-          <button className="button-quarter" type="submit">
-            Submit
-          </button>
-        </form>
-      </div>
-    );
-  }
-);
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+});
